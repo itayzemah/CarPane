221be9b4a16a5d7136e86522d6b8a85024d68c64
itayzemah <38214666+itayzemah@users.noreply.github.com>
2019-03-09 23:17:33 +0200

    update until sixth row

--- CarPane.java
+++ CarPane.java
@@ -1,11 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import javax.swing.text.html.MinimalHTMLWriter;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -15,42 +16,21 @@ import javafx.stage.Stage;
 public class CarPane extends Application implements CarPaneFinals {
 	private Stage carStage;
 	private Scene carScene;
-	private ArrayList<TextField> textFields;
-	private ArrayList<HBox>	hboxes;
-//	private TextField carIDTF;
-//	private HBox carIDBox;
-//	private Label carIDL;
-//	private TextField dateTF;
-//	private HBox dateBox;
-//	private Label dateL;
-//	private TextField chassisTF;
-//	private HBox chassisbox;
-//	private Label cahssisL;
-//	private TextField yearTF;
-//	private HBox yearBox;
-//	private Label yearL;
-//	private TextField monthTF;
-//	private HBox monthBox;
-//	private Label monthL;
-//	private TextField dayTF;
-//	private HBox dayBox;
-//	private Label dayL;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		carStage = new Stage();
 		BorderPane layout = new BorderPane();
 		carScene = new Scene(layout);
 		carStage.setTitle("רכב");
-//		createLists();
 		GridPane center = new GridPane();
 		enterFieldsToCenter(center);
-		
-		
+
 		layout.setCenter(center);
 		carStage.setScene(carScene);
 		sizeOfStage();
 		carStage.show();
-		
+
 	}
 
 	private void sizeOfStage() {
@@ -62,29 +42,111 @@ public class CarPane extends Application implements CarPaneFinals {
 	}
 
 	private void enterFieldsToCenter(GridPane center) {
-	control_TF_size();
-	center.add(combine(carIDTF,carIDL), 3, 0);
-	center.add(combine(statusCB,statusL),2,0);
-	center.add(combine(chassisTF,cahssisL), 9, 0);
-//	center.add(,2,1);
-	center.add(combine(combine(combine(dayTF,dayL),combine(monthTF,monthL)),combine(yearTF,yearL)),3,1);
-//	center.add(,4,1);	
+		control_TF_size();
+		center.setAlignment(Pos.TOP_CENTER);
+		center.setHgap(10);
+		center.setVgap(10);
+		center.setPadding(new Insets(10));
+
+		firstRow(center);
+		
+		secondRow(center);
+		
+		thirdRow(center);
+		
+		fourthRow(center);
+		
+		fifthRow(center);
+
+	}
+
+	private void fifthRow(GridPane center) {
+		center.add(combine(KMTF,KML),0,4);
+		center.add(combine(HPTF,HPL),1,4);
+		center.add(combine(tireSizeTF,tireSizeL),2,4);
+		center.add(combine(combine(airBagsTF, airBagsL),combine(pollutionTF, pollutionL)),3,4);
+		center.add(combine(isProtection,IsSunRoof,isAutoMatic),4,4);
+		
+		suit5Row();
+	}
+
 	
-}
+
+	private void suit5Row() {
+		KMTF.setMaxWidth(_10CHARS_TF);
+		HPTF.setMaxWidth(_4_5_CHARS_TF);
+		airBagsTF.setMaxWidth(_2CHARS_TF);
+		pollutionTF.setMaxWidth(_2CHARS_TF);
+	}
+
+	private void fourthRow(GridPane center) {
+		center.add(combine(monthlypriceTF,montlypriceL),0,3);
+		center.add(combine(testpriceTF,testpriceL), 1, 3);
+		center.add(combine(testDateTF,testpDateL), 2, 3);
+		center.add(combine(combine(numOfDoorsTF,numOfDoorsL),combine(numOfOwnersTF, numOfOwnersL)), 3, 3);
+		center.add(combine(combine(combine(dayTF, dayL), combine(monthTF, monthL)), combine(yearTF, yearL)), 4, 3);
+	}
+
+	private void thirdRow(GridPane center) {
+		center.add(combine(shopPriceTF,shopPriceL),0,2);
+		center.add(combine(originallyCB,originallyL),1,2);
+		center.add(ownersList,2,2);
+		center.add(ownerDetails,3,2);
+		center.add(combine(ownersCB,ownersL),4,2,2,1);
+		
+	}
+
+	private void secondRow(GridPane center) {
+		center.add(combine(listPriceTF,listPriceL),0,1);
+		center.add(combine(colorsCB,colorsL),1,1);
+		colorsCB.setEditable(true);
+		new AutoCompleteComboBoxListener<>(colorsCB);
+		center.add(modelsList, 3, 1);
+		center.add(combine(modelsCB, modelsL), 4, 1,2,1);
+		modelsCB.setEditable(true);
+		new AutoCompleteComboBoxListener<>(modelsCB);
+	}
+
+	private void firstRow(GridPane center) {
+		center.add(combine(chassisTF, cahssisL), 0, 0);
+		center.add(combine(statusCB, statusL), 1, 0);
+		statusCB.setEditable(true);
+		new AutoCompleteComboBoxListener<String>(statusCB);
+		center.add(onLine,2,0);
+		center.add(combine(carIDTF, carIDL), 3, 0);
+		center.add(combine(dateModifyTF,dateModifyL) ,4,0,2,1);
+	}
 
 	private void control_TF_size() {
 		yearTF.setMaxWidth(_4_5_CHARS_TF);
 		monthTF.setMaxWidth(_2CHARS_TF);
 		dayTF.setMaxWidth(_2CHARS_TF);
+		carIDTF.setMaxWidth(_10CHARS_TF);
+		modelsCB.setMinWidth(400);
+		ownersCB.setMinWidth(400);
+		testDateTF.setMaxWidth(_10CHARS_TF);
+		testpriceTF.setMaxWidth(_4_5_CHARS_TF);
+		numOfDoorsTF.setMaxWidth(_2CHARS_TF);
+		numOfOwnersTF.setMaxWidth(_2CHARS_TF);
+		listPriceTF.setMaxWidth(_10CHARS_TF);
+		shopPriceTF.setMaxWidth(_10CHARS_TF);
+		chassisTF.setMaxWidth(_10CHARS_TF);
+		monthlypriceTF.setMaxWidth(_10CHARS_TF);
 	}
 
-	private HBox combine(Node textField, Node label1) {
+	private HBox combine(Node node1, Node node2) {
 		HBox hb = new HBox();
-		hb.getChildren().addAll(textField,label1);
+		hb.getChildren().addAll(node1, node2);
 		hb.setSpacing(5);
 		return hb;
 	}
 
+	private Node combine(CheckBox node1, CheckBox node2, CheckBox node3) {
+		HBox hb = new HBox();
+		hb.getChildren().addAll(node1, node2,node3);
+		hb.setSpacing(5);
+		return hb;
+	}
 //	private void createLists() {
 //		textFields.addAll(Arrays.asList(tFields));
 //		hboxes.addAll(Arrays.asList(boxes));
--- CarPaneFinals.java
+++ CarPaneFinals.java
@@ -2,39 +2,114 @@ import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public interface CarPaneFinals {
+	
+	// --- first row nodes-------
 	TextField carIDTF = new TextField();
-	HBox carIDBox = new HBox();
 	Label carIDL = new Label("מספר רישוי");
-	TextField dateTF = new TextField();
-	HBox dateBox = new HBox();
-	Label dateL = new Label("תאריך");
+	
 	TextField chassisTF = new TextField();
-	HBox chassisbox = new HBox();
 	Label cahssisL = new Label("שלדה");
-	TextField yearTF = new TextField();
-	HBox yearBox = new HBox();
-	Label yearL = new Label("עליה לכביש:  שנה");
-	TextField monthTF= new TextField();
-	HBox monthBox = new HBox();
-	Label monthL = new Label("חודש");
-	TextField dayTF = new TextField();
-	HBox dayBox = new HBox();
-	Label dayL = new Label("יום");
+
 	ObservableList<String> status = FXCollections.observableArrayList("נתניה", "הרצליה", "outlet");
 	ComboBox<String> statusCB = new ComboBox<>(status);
 	Label statusL = new Label("סטטוס");
+	
+	CheckBox onLine = new CheckBox("מוצג באתר");
+	
+	Label dateModifyL = new Label("תאריך כניסה");
+	TextField dateModifyTF = new TextField();
+	
+	
+	//----- second row nodes----
+	Label listPriceL = new Label("מחירון");
+	TextField listPriceTF = new TextField();
+	
+	ObservableList<String> colors = FXCollections.observableArrayList("שחור", "לבן", "אדום","אפור","ברונזה","לבן פנינה");
+	ComboBox<String> colorsCB = new ComboBox<>(colors);
+	Label colorsL = new Label("צבע");
+	
+	Button modelsList = new Button("רשימת מודלים");
+	
+	ObservableList<String> models = FXCollections.observableArrayList();
+	ComboBox<String> modelsCB = new ComboBox<>(models);
+	Label modelsL = new Label("דגם");
+	
+	
+	//------- third row nodes---------
+	Label shopPriceL = new Label("מחיר");
+	TextField shopPriceTF = new TextField();
+	
+	ObservableList<String> originally = FXCollections.observableArrayList("פרטי", "ליסינג מימוני","ליסינג תפעולי","השכרה","חברה");
+	ComboBox<String> originallyCB = new ComboBox<>(originally);
+	Label originallyL = new Label("מקוריות");
+	
+	Button ownersList = new Button("רשימת לקוחות");
+	Button ownerDetails = new Button("פרטי לקוח");
+	
+	ObservableList<String> owners = FXCollections.observableArrayList();
+	ComboBox<String> ownersCB = new ComboBox<>(owners);
+	Label ownersL = new Label("בעלים");
+	
+	//------- fourth row nodes---------
+	TextField yearTF = new TextField();
+	Label yearL = new Label("עליה לכביש:  שנה");
+
+	TextField monthTF= new TextField();
+	Label monthL = new Label("חודש");
+
+	TextField dayTF = new TextField();
+	Label dayL = new Label("יום");
+	
+	TextField monthlypriceTF = new TextField();
+	Label montlypriceL = new Label("מחיר חודשי");
+	
+	TextField testpriceTF = new TextField();
+	Label testpriceL = new Label("סכום טסט");
+	
+	TextField testDateTF = new TextField();
+	Label testpDateL = new Label("תאריך טסט");
+	
+	TextField numOfDoorsTF = new TextField();
+	Label numOfDoorsL = new Label("דלתות");
+	
+	TextField numOfOwnersTF = new TextField();
+	Label numOfOwnersL = new Label("מ.בעלים");
+	
+	//-------- fifth row nodes -----------
+	TextField KMTF = new TextField();
+	Label KML = new Label("מד אוץ");
+	
+	TextField HPTF = new TextField();
+	Label HPL = new Label("כ\"ס");
+	
+	TextField tireSizeTF = new TextField();
+	Label tireSizeL = new Label("צמיגים");
+	
+	TextField airBagsTF = new TextField();
+	Label airBagsL = new Label("כריות אוויר");
+	
+	TextField pollutionTF = new TextField();
+	Label pollutionL = new Label("זיהום אוויר");
+	
+	CheckBox isProtection = new CheckBox("מיגון");
+	CheckBox IsSunRoof = new CheckBox("חלון בגג");
+	CheckBox isAutoMatic = new CheckBox("אוטומט");
+	
+	
 	ArrayList<TextField> textFields = new ArrayList<>();
 	ArrayList<HBox>	hboxes = new ArrayList<>();
 	ArrayList<Label> labels = new ArrayList<>();
-	TextField[] tFields = new TextField[] {carIDTF,dateTF, chassisTF,yearTF,monthTF,dayTF};
-	HBox[] boxes = new HBox[] {carIDBox,dateBox,chassisbox,yearBox,monthBox,dayBox};
+//	TextField[] tFields = new TextField[] {carIDTF,dateTF, chassisTF,yearTF,monthTF,dayTF};
+//	HBox[] boxes = new HBox[] {carIDBox,dateBox,chassisbox,yearBox,monthBox,dayBox};
 	final double _4_5_CHARS_TF = 50;
 	final double _2CHARS_TF = 30;
-	
+	final double _10CHARS_TF = 90;
 }
